df5f395e3ffb7c023fb1c4b9d79cdf12c45d03d2
Dave Seidman <daveseidman@Daves-MacBook-Pro.local>
2023-04-29 11:23:35 -0400

    fix frustrum cull

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,32 @@
 import React, { useRef } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Canvas, useFrame } from '@react-three/fiber';
-import { AnimationMixer } from 'three';
+import { AnimationMixer, BoxGeometry, MeshNormalMaterial } from 'three';
 import { OrbitControls, useGLTF, Environment, PerspectiveCamera } from '@react-three/drei';
 import { EffectComposer, DepthOfField, Bloom, Noise, Vignette } from '@react-three/postprocessing';
 import './App.scss';
 import model from './assets/model3.glb';
 import envMap from './assets/modern_buildings_2k.hdr';
 
-function Model() {
+function Model(props) {
+  const { scale } = props;
   let mixer = null;
   const { scene, animations } = useGLTF(model);
+  scene.traverse((obj) => { obj.frustumCulled = false; });
   mixer = new AnimationMixer(scene);
   mixer.clipAction(animations[1]).play();
   useFrame((state, delta) => {
     mixer.update(delta);
   });
-  return <primitive object={scene} position={[0, 0, 0]} />;
+  return <primitive object={scene} position={[0, 0, 0]} scale={scale} />;
 }
+
+const pages = [
+  'About',
+  'Experience',
+  'Work',
+  'Contact',
+];
 
 const App = () => {
   const camTarget = useRef();
@@ -28,25 +37,25 @@ const App = () => {
         <Canvas
           shadows={{ type: 'PCFShadowMap' }}
         >
-
           <PerspectiveCamera
             makeDefault
             far={1100}
             near={0.1}
-            position={[0, 1, 5]}
+            position={[0, 1, 2.5]}
+            frustumCulled
           />
           <OrbitControls
             enablePan={false}
-            maxPolarAngle={Math.PI * 0.5}
-            minPolarAngle={Math.PI * 0.5}
+            maxPolarAngle={Math.PI / 2 + 0.1}
+            minPolarAngle={Math.PI / 2 - 0.1}
             minDistance={0.25}
             maxDistance={2.5}
             target-y={1}
           />
           <EffectComposer>
-            <DepthOfField focusDistance={5} focalLength={10} bokehScale={3} height={400} />
+            {/* <DepthOfField focusDistance={5} focalLength={10} bokehScale={3} height={400} /> */}
             {/* <BokehEffect /> */}
-            <Bloom luminanceThreshold={1} luminanceSmoothing={0.9} height={300} />
+            <Bloom luminanceThreshold={0.7} luminanceSmoothing={0.9} height={300} />
             {/* <Noise opacity={0.02} /> */}
             <Vignette eskil={false} offset={0} darkness={0.5} />
             <pointLight intensity={1} position={[2, 10, 20]} />
@@ -55,8 +64,8 @@ const App = () => {
               exposure={5}
               ground={{ height: 1, radius: 8 }}
             />
-            <group position={[0, 0, 0]} scale={1}>
-              <Model />
+            <group position={[0, 0, 0]} scale={0.5}>
+              <Model scale={2} />
             </group>
           </EffectComposer>
         </Canvas>
