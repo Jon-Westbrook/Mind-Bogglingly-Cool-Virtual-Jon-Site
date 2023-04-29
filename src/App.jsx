ab01cd26c8033b40f0240a38ab60088983a7aebc
Dave Seidman <daveseidman@Daves-MacBook-Pro.local>
2023-04-29 19:51:04 -0400

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import React, { useRef, useState } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Canvas, useFrame } from '@react-three/fiber';
 import { AnimationMixer, MathUtils, Vector3 } from 'three';
-import { OrbitControls, useGLTF, Environment, PerspectiveCamera } from '@react-three/drei';
-import { EffectComposer, DepthOfField, Bloom, Noise, Vignette } from '@react-three/postprocessing';
+import { OrbitControls, useGLTF, Environment, PerspectiveCamera, CameraShake } from '@react-three/drei';
+import { EffectComposer, DepthOfField, Bloom, Noise, Vignette, ChromaticAberration, Depth } from '@react-three/postprocessing';
 import './App.scss';
-import model from './assets/model3.glb';
+import model from './assets/model4.glb';
 import envMap from './assets/modern_buildings_2k.hdr';
 import pages from './assets/pages.json';
 
@@ -33,19 +33,20 @@ const App = () => {
   const [cameraTarget, setCameraTarget] = useState([0, 1, 0]);
   const [lerpedCameraTarget, setLerpedCameraTarget] = useState(cameraTarget);
   const [cameraRotate, setCameraRotate] = useState(true);
-
+  const [camMoving, setCamMoving] = useState(false);
 
   function CamController() {
     useFrame((state, delta) => {
-      const camX = MathUtils.damp(cameraPosition[0], state.camera.position.x, 500, delta);
-      const camY = MathUtils.damp(cameraPosition[1], state.camera.position.y, 500, delta);
-      const camZ = MathUtils.damp(cameraPosition[2], state.camera.position.z, 500, delta);
-      const tarX = MathUtils.damp(cameraTarget[0], controlsRef.current.target.x, 500, delta);
-      const tarY = MathUtils.damp(cameraTarget[1], controlsRef.current.target.y, 500, delta);
-      const tarZ = MathUtils.damp(cameraTarget[2], controlsRef.current.target.z, 500, delta);
-
-      setLerpedCameraPosition([camX, camY, camZ]);
-      setLerpedCameraTarget([tarX, tarY, tarZ]);
+      if (camMoving) {
+        const camX = MathUtils.damp(cameraPosition[0], state.camera.position.x, 400, delta);
+        const camY = MathUtils.damp(cameraPosition[1], state.camera.position.y, 400, delta);
+        const camZ = MathUtils.damp(cameraPosition[2], state.camera.position.z, 400, delta);
+        const tarX = MathUtils.damp(cameraTarget[0], controlsRef.current.target.x, 400, delta);
+        const tarY = MathUtils.damp(cameraTarget[1], controlsRef.current.target.y, 400, delta);
+        const tarZ = MathUtils.damp(cameraTarget[2], controlsRef.current.target.z, 400, delta);
+        setLerpedCameraPosition([camX, camY, camZ]);
+        setLerpedCameraTarget([tarX, tarY, tarZ]);
+      }
     });
   }
 
@@ -53,6 +54,8 @@ const App = () => {
   const navigate = ({ target }) => {
     const page = pages.find(({ name }) => name === target.innerText);
     const { camPostion, camTarget, camRotate } = page;
+    setCamMoving(true);
+    setTimeout(() => { setCamMoving(false); }, 800);
 
     setCurrentPage(target.innerText);
     setCameraPosition(camPostion);
@@ -80,16 +83,19 @@ const App = () => {
             ref={controlsRef}
             enablePan={false}
             enableRotate={cameraRotate}
-            maxPolarAngle={Math.PI / 2 + 0.1}
-            minPolarAngle={Math.PI / 2 - 0.1}
+            // maxPolarAngle={Math.PI / 2 + 0.1}
+            // minPolarAngle={Math.PI / 2 - 0.1}
             minDistance={0.25}
             maxDistance={5}
             target={lerpedCameraTarget}
           />
           <EffectComposer>
-            {/* <DepthOfField focusDistance={5} focalLength={10} bokehScale={3} height={400} /> */}
+            <ChromaticAberration
+              offset={[0.005, 0.001]}
+            />
+            <DepthOfField focusDistance={0.1} focalLength={1} bokehScale={20} height={400} />
             {/* <BokehEffect /> */}
-            <Bloom luminanceThreshold={0.7} luminanceSmoothing={0.9} height={300} />
+            {/* <Bloom luminanceThreshold={0.7} luminanceSmoothing={0.9} height={300} /> */}
             {/* <Noise opacity={0.02} /> */}
             <Vignette eskil={false} offset={0} darkness={0.5} />
             <pointLight intensity={1} position={[2, 10, 20]} />
