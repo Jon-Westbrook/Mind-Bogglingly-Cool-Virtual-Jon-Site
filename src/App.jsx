ea23499279db9cf351bfc9624a77ac28faddfb9b
Dave Seidman <daveseidman@Daves-MacBook-Pro.local>
2023-06-11 13:55:35 -0400

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Canvas, useFrame, useThree } from '@react-three/fiber';
-import { AnimationMixer, BoxGeometry, VSMShadowMap, PCFSoftShadowMap, MeshPhysicalMaterial, MeshStandardMaterial, Object3D, Vector3, SRGBColorSpace, PlaneGeometry } from 'three';
+import { AnimationMixer, PCFSoftShadowMap, MeshPhysicalMaterial } from 'three';
 import { OrbitControls, useGLTF, Environment, PerspectiveCamera } from '@react-three/drei';
-import { EffectComposer, DepthOfField, Bloom, Noise, Vignette, ChromaticAberration } from '@react-three/postprocessing';
+import { EffectComposer, DepthOfField, Bloom, Vignette, ChromaticAberration } from '@react-three/postprocessing';
 import { useSpring, animated, config } from '@react-spring/three';
 import './App.scss';
-import model from './assets/model6.glb';
+import model from './assets/model.glb';
 import envMap from './assets/modern_buildings_2k.hdr';
 import pages from './assets/pages.json';
 
 
 function Model() {
-  // console.log(props.scene);
   const state = useThree();
 
   let mixer = null;
@@ -24,8 +23,6 @@ function Model() {
       obj.material.envMap = state.scene.environment;
       obj.material.emissiveIntensity = 3;
       obj.material.needsUpdate = true;
-      // console.log(obj.material);
-      // console.log(state.scene.environment);
     }
     if (obj.name === 'Wolf3D_Glasses') {
       console.log(obj);
@@ -84,7 +81,7 @@ const App = () => {
       <div className="app">
         <Canvas
           shadows={{ type: PCFSoftShadowMap }}
-          dpr={0.66}
+          dpr={0.5}
           ref={canvasRef}
           // gl-outputColorSpace={SRGBColorSpace}
         >
@@ -104,49 +101,29 @@ const App = () => {
             maxDistance={5}
           />
           <EffectComposer>
-            {/* <ChromaticAberration offset={[0.0025, 0.001]} /> */}
-            {/* <DepthOfField focusDistance={0.09} focalLength={0.3} bokehScale={4} height={1000} /> */}
-            {/* <Bloom luminanceThreshold={0.7} luminanceSmoothing={0.9} height={300} /> */}
+            <ChromaticAberration offset={[0.0004, 0.0004]} />
+            <DepthOfField focusDistance={0.25} focalLength={2.5} bokehScale={16} height={1024} />
+            <Bloom luminanceThreshold={0.8} luminanceSmoothing={0.9} height={300} />
             {/* <Noise opacity={1} intensity={200} /> */}
-            {/* <Vignette eskil={false} offset={0} darkness={0.5} /> */}
+            <Vignette eskil={false} offset={0} darkness={0.5} />
             <directionalLight
-              intensity={2}
+              intensity={3}
               castShadow
               shadow-mapSize={1024 * 2}
               shadow-bias={0.000001}
-              // shadow-normalBias={-0.00001}
-              // shadow-radius={50}
-              // shadow-blurSamples={205}
               position={[1, 2, 1]}
               target-position={[0, 20, 0]}
 
             />
             <Environment
-              // ref={envRef}
               files={envMap}
               background
-              // blur={0.08}
+              // blur={0.01}
               exposure={1}
-              // ref={env}
               // ground={{ height: 1, radius: 8 }}
             />
             <group>
               <Model />
-              {/* <mesh
-                geometry={new BoxGeometry(0.75, 0.5, 0.75)}
-                material={new MeshStandardMaterial({ color: 'red' })}
-                castShadow
-                receiveShadow
-              /> */}
-              <mesh
-                geometry={new PlaneGeometry(3, 3)}
-                position={[0, 0, 0]}
-                rotation={[-Math.PI / 2, 0, 0]}
-                castShadow
-                receiveShadow
-              >
-                <shadowMaterial />
-              </mesh>
             </group>
           </EffectComposer>
         </Canvas>
