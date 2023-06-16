84b64667140128ee22342e0a36352fa72a50b46f
Jonathon Westbrook <jonathon.westbrook@gmail.com>
2023-06-16 17:56:57 -0400

    temp solution to get camPosition working, plus some style cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -14,11 +14,11 @@ function Pages(props) {
   const location = useLocation();
   const { setCurrentPage } = props;
   useEffect(() => {
-    const page = location.pathname.split('/')[2];
+    const page = location.pathname.split('/')[2] || 'Home';
     setCurrentPage(page);
   }, [location]);
 
-  return (<div />);
+  return <div />;
 }
 
 function Model(props) {
@@ -58,7 +58,7 @@ function Model(props) {
     setCurrentPage('Home');
   }, [scene]);
 
-  return (<primitive object={scene} />);
+  return <primitive object={scene} />;
 }
 
 const App = () => {
@@ -68,38 +68,36 @@ const App = () => {
   const controlsRef = useRef();
   const canvasRef = useRef();
 
-  const { animatedPosition } = useSpring({
-    animatedPosition: pages.find(p => p.name === currentPage).camPostion,
-    config: config.slow,
-    onChange: (e) => {
-      if (cameraRef.current) cameraRef.current.position.set(e.value.animatedPosition[0], e.value.animatedPosition[1], e.value.animatedPosition[2]);
+  const { animatedPosition } = useSpring(
+    {
+      animatedPosition: pages.find((p) => p.name === currentPage).camPostion,
+      config: config.slow,
+      onChange: (e) => {
+        if (cameraRef.current) cameraRef.current.position.set(e.value.animatedPosition[0], e.value.animatedPosition[1], e.value.animatedPosition[2]);
+      },
     },
-  }, [currentPage]);
-  const { animatedTarget } = useSpring({
-    animatedTarget: pages.find(p => p.name === currentPage).camTarget,
-    config: config.slow,
-    onChange: (e) => {
-      if (controlsRef.current) { controlsRef.current.target.set(e.value.animatedTarget[0], e.value.animatedTarget[1], e.value.animatedTarget[2]); }
+    [currentPage]
+  );
+  const { animatedTarget } = useSpring(
+    {
+      animatedTarget: pages.find((p) => p.name === currentPage).camTarget,
+      config: config.slow,
+      onChange: (e) => {
+        if (controlsRef.current) {
+          controlsRef.current.target.set(e.value.animatedTarget[0], e.value.animatedTarget[1], e.value.animatedTarget[2]);
+        }
+      },
     },
-  }, [currentPage]);
+    [currentPage]
+  );
 
   return (
     <Router>
       <Pages setCurrentPage={setCurrentPage} />
-      <div className="app">
-        <Canvas
-          shadows={{ type: PCFSoftShadowMap }}
-          dpr={0.5}
-          ref={canvasRef}
-        >
+      <div className='app'>
+        <Canvas shadows={{ type: PCFSoftShadowMap }} dpr={0.5} ref={canvasRef}>
           <animated.group>
-            <PerspectiveCamera
-              makeDefault
-              far={1100}
-              near={0.1}
-              fov={20}
-              ref={cameraRef}
-            />
+            <PerspectiveCamera makeDefault far={1100} near={0.1} fov={20} ref={cameraRef} />
           </animated.group>
           <OrbitControls
             ref={controlsRef}
@@ -113,15 +111,7 @@ const App = () => {
             <Bloom luminanceThreshold={0.8} luminanceSmoothing={0.9} height={300} />
             {/* <Noise opacity={1} intensity={200} /> */}
             <Vignette eskil={false} offset={0} darkness={0.5} />
-            <directionalLight
-              intensity={3}
-              castShadow
-              shadow-mapSize={1024 * 2}
-              shadow-bias={0.000001}
-              position={[1, 2, 1]}
-              target-position={[0, 20, 0]}
-
-            />
+            <directionalLight intensity={3} castShadow shadow-mapSize={1024 * 2} shadow-bias={0.000001} position={[1, 2, 1]} target-position={[0, 20, 0]} />
             <Environment
               files={envMap}
               background
@@ -130,24 +120,20 @@ const App = () => {
               // ground={{ height: 1, radius: 8 }}
             />
             <group>
-              <Model
-                setCurrentPage={setCurrentPage}
-              />
+              <Model setCurrentPage={setCurrentPage} />
             </group>
           </EffectComposer>
         </Canvas>
-        <ul className="nav">
-          {
-          pages.map(page => (
+        <ul className='nav'>
+          {pages.map((page) => (
             <li key={page.name}>
               {/* <button type="button" onClick={navigate}>{page.name}</button> */}
-              <Link to={`/jonwest/${page.name}`}>{page.name}</Link>
+              <Link to={`/${page.name}`}>{page.name}</Link>
             </li>
-          ))
-          }
+          ))}
           {/* <button onClick={changeLight}>ChangeLight</button> */}
         </ul>
-        <h1 className="pageTitle">{currentPage}</h1>
+        <h1 className='pageTitle'>{currentPage}</h1>
       </div>
     </Router>
   );
