a524a01d617f602265eea23d8dd873e5f5e8bfbc
Jonathon Westbrook <jonathon.westbrook@gmail.com>
2023-09-15 19:21:41 -0400

    converts dynamic camera positioning and targeting to use currentPage (view) settings

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,8 @@ import React, { useEffect, useRef, useState } from 'react';
 import { Link, BrowserRouter as Router, useLocation } from 'react-router-dom';
 import { Canvas, useFrame } from '@react-three/fiber';
 import { AnimationMixer, PCFSoftShadowMap } from 'three';
-import { OrbitControls, useGLTF, Environment, PerspectiveCamera } from '@react-three/drei';
+import { useGLTF, Environment, PerspectiveCamera } from '@react-three/drei';
 import { EffectComposer, DepthOfField, Bloom, Vignette, ChromaticAberration } from '@react-three/postprocessing';
-import { useSpring, animated, config } from '@react-spring/three';
 import './App.scss';
 import model from './assets/models/model2.glb';
 import envMap from './assets/img/environments/hamburg_canal_2k.hdr';
@@ -34,61 +33,60 @@ function Model() {
   return <primitive object={scene} />;
 }
 
+function Camera(props) {
+  const { camPosition, camTarget, currentPage } = props;
+  const cameraRef = useRef();
+
+  useEffect(() => {
+    if (cameraRef.current && camTarget && camPosition) {
+      cameraRef.current.position.set(camPosition[0], camPosition[1], camPosition[2]);
+      cameraRef.current.lookAt(camTarget[0], camTarget[1], camTarget[2]);
+    }
+  }, [camPosition, camTarget, currentPage]);
+
+  return (
+    <PerspectiveCamera
+      makeDefault
+      far={1100}
+      near={0.1}
+      fov={20}
+      ref={cameraRef}
+    />
+  );
+}
+
 const App = () => {
   const [currentPage, setCurrentPage] = useState('View1');
-  const cameraRef = useRef();
-  const controlsRef = useRef();
+  const [currentCamPosition, setCurrentCamPosition] = useState([0, 1.7, 2]);
+  const [currentCamTarget, setCurrentCamTarget] = useState([0, 1.55, 0]);
   const canvasRef = useRef();
 
-  const { animatedPosition } = useSpring(
-    {
-      animatedPosition: pages.find(p => p.name === currentPage).camPostion,
-      config: config.slow,
-      onChange: e => {
-        if (cameraRef.current) cameraRef.current.position.set(e.value.animatedPosition[0], e.value.animatedPosition[1], e.value.animatedPosition[2]);
-      },
-    },
-    [currentPage]
-  );
-
-  const { animatedTarget } = useSpring(
-    {
-      animatedTarget: pages.find(p => p.name === currentPage).camTarget,
-      config: config.slow,
-      onChange: e => {
-        if (controlsRef.current) {
-          controlsRef.current.target.set(e.value.animatedTarget[0], e.value.animatedTarget[1], e.value.animatedTarget[2]);
-        }
-      },
-    },
-    [currentPage]
-  );
-
+  useEffect(() => {
+    if (currentPage) {
+      const { camPosition, camTarget } = pages.find(page => page.name === currentPage);
+      console.log(camPosition, camTarget);
+      setCurrentCamPosition(camPosition);
+      setCurrentCamTarget(camTarget);
+    }
+  }, [currentPage]);
 
   return (
     <Router>
       <Pages setCurrentPage={setCurrentPage} />
-
       <div className="app">
         <Canvas shadows={{ type: PCFSoftShadowMap }} dpr={0.5} ref={canvasRef}>
 
-          <animated.group>
-            <PerspectiveCamera makeDefault far={1100} near={0.1} fov={20} ref={cameraRef} />
-          </animated.group>
-
-          <OrbitControls
-            ref={controlsRef}
-            // enablePan={false}
-            minDistance={0.25}
-            maxDistance={5}
+          <Camera
+            camPosition={currentCamPosition}
+            camTarget={currentCamTarget}
           />
 
           <EffectComposer>
-            <ChromaticAberration offset={[0.0004, 0.0004]} />
-            <DepthOfField focusDistance={0.1} focalLength={3} bokehScale={16} height={1024} />
-            <Bloom luminanceThreshold={0.9} luminanceSmoothing={0.9} height={300} />
-            <Vignette eskil={false} offset={0} darkness={0.5} />
             <directionalLight intensity={3} castShadow shadow-mapSize={1024 * 2} shadow-bias={0.000001} position={[1, 2, 1]} target-position={[0, 20, 0]} />
+            {/* <ChromaticAberration offset={[0.0004, 0.0004]} /> */}
+            {/* <DepthOfField focusDistance={0.1} focalLength={3} bokehScale={16} height={1024} /> */}
+            {/* <Bloom luminanceThreshold={0.9} luminanceSmoothing={0.9} height={300} /> */}
+            {/* <Vignette eskil={false} offset={0} darkness={0.5} /> */}
 
             <Environment
               files={envMap}
@@ -107,7 +105,11 @@ const App = () => {
         <ul className="nav">
           {pages.map(page => (
             <li key={page.name}>
-              <Link to={`/${page.name}`}>{page.name}</Link>
+              <Link
+                to={`/${page.name}`}
+                className={currentPage === page.name ? 'active-link' : ''}
+              >{page.name}
+              </Link>
             </li>
           ))}
         </ul>
