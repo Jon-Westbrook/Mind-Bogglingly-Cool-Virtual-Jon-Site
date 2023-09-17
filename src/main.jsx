fa3aeb1da3ed6ead14ee145847b1d465829a67df
Jonathon Westbrook <jonathon.westbrook@gmail.com>
2023-09-17 13:59:02 -0400

    cleanup main

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import { mountStoreDevtool } from 'simple-zustand-devtools';
-// import useStore from './store';
-import inobonce from 'inobounce'
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />,
+  <App />
 );
-// mountStoreDevtool('Store', useStore);
