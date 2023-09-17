bd7428d7a5a1bd1fbdff69d5fe6e58fa607ac2d6
Jonathon Westbrook <jonathon.westbrook@gmail.com>
2023-09-17 00:34:17 -0400

    attempting the public folder option for social-sharing images

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.glb', '**/*.gltf', '**/*.hdr'],
+  assetsInclude: ['**/*.glb', '**/*.gltf', '**/*.hdr', '**/*.jpg'],
   server: {
     port: 8080,
     host: true,
