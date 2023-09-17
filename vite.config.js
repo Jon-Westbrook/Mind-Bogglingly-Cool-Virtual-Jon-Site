4c89410b7619166f3e04bef44b597beaa056f87d
Jonathon Westbrook <jonathon.westbrook@gmail.com>
2023-09-17 17:14:05 -0400

    open site on start, let's get chunky

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,44 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.glb', '**/*.gltf', '**/*.hdr', '**/*.jpg'],
+  assetsInclude: ['**/*.glb', '**/*.gltf', '**/*.hdr'],
   server: {
     port: 8080,
     host: true,
+    open: '/',
   },
+  build: {
+    cssCodeSplit: false,
+    chunkSizeWarningLimit: 650,
+    rollupOptions: {
+      external: [
+        'react',
+        'react-dom',
+        'react-router-dom',
+        '@react-three/fiber',
+        '@react-three/drei',
+        'three'],
+      output: {
+        format: 'es',
+        manualChunks(id) {
+          if (id.includes('node_modules/react/')) {
+            return 'react';
+          }
+          if (id.includes('node_modules/react-dom/')) {
+            return 'react-dom';
+          }
+          if (id.includes('node_modules/react-router-dom/')) {
+            return 'react-router-dom';
+          }
+          if (id.includes('node_modules/@react-three/')) {
+            return 'react-three';
+          }
+          if (id.includes('node_modules/three/')) {
+            return 'three';
+          }
+          return 'vendor'; // misc prefix if not matching any condition
+        }
+      }
+    }
+  }
 });
