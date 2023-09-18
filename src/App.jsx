21f3010d939de7f2f86ab41bb588ddd8937dd0d4
Jonathon Westbrook <jonathon.westbrook@gmail.com>
2023-09-18 16:37:53 -0400

    working jsx-jish model

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,9 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Canvas } from '@react-three/fiber';
 import { PCFSoftShadowMap } from 'three';
 import Loader from './Loader';
-import Model from './Model';
+import Model from './assets/models/Model3.jsx'; 
 import './styles/App.scss';
+import { OrbitControls } from '@react-three/drei';
 
 function App() {
   const canvasRef = useRef();
@@ -13,7 +14,6 @@ function App() {
   useEffect(() => {    
     if (loaderUnmounted) {
       canvasRef.current.classList = 'fade-in';
-      console.log(canvasRef.current.classList);
     }
   }, [loaderUnmounted]);
 
@@ -23,6 +23,7 @@ function App() {
         <Suspense fallback={<Loader onUnmount={() => setLoaderUnmounted(true)} />}> 
           <div className="app">
             <Canvas shadows={{ type: PCFSoftShadowMap }} dpr={1} ref={canvasRef}>
+              <OrbitControls />
               <Model />
             </Canvas>
           </div>
