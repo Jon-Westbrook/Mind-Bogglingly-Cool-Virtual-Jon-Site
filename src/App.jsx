48336830b5d826c51b9576edf4f607308e2d270e
Jonathon Westbrook <jonathon.westbrook@gmail.com>
2023-09-19 10:22:21 -0400

    puts EffectsComposer last for performance

--- src/App.jsx
+++ src/App.jsx
@@ -86,27 +86,27 @@ function App() {
                 camPosition={currentCamPosition}
                 camTarget={currentCamTarget}
               />
+              <PresentationControls
+                enabled
+                global={true}
+                cursor={false}
+                snap={true}
+                speed={6}
+                zoom={1}
+                polar={[0, 0.1]}
+                azimuth={[-Infinity, Infinity]}
+                config={{ mass: 1, tension: 170, friction: 26 }}
+              ><Model />
+              </PresentationControls>
+              <Environment
+                files={envMap}
+                background
+                exposure={1}
+                blur={0}
+              />
               <EffectComposer>
                 <directionalLight intensity={0.5} castShadow shadow-mapSize={1024 * 2} shadow-bias={0.000001} position={[0, 1.7, 0]} target-position={[0, 1.55, 0]} />
                 <Vignette eskil={false} offset={0} darkness={0.9} />
-                <Environment
-                  files={envMap}
-                  background
-                  exposure={1}
-                  blur={0}
-                />
-                <PresentationControls
-                  enabled
-                  global={true}
-                  cursor={false}
-                  snap={true}
-                  speed={8}
-                  zoom={1}
-                  polar={[0, 0.1]}
-                  azimuth={[-Infinity, Infinity]}
-                  config={{ mass: 1, tension: 170, friction: 26 }}
-                ><Model />
-                </PresentationControls>
               </EffectComposer>
             </Canvas>
           </div>
